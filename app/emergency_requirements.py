24835af5b514b12ea5f2a7471d2f45e6c95e4af1
bobby <contact@bobbyobrien.com>
2025-02-16 10:53:18 -0600

    Modified emergency requirements.

--- app/emergency_requirements.py
+++ app/emergency_requirements.py
@@ -5,14 +5,14 @@ class EmergencyRequirement:
     self.type_to_quantity = type_to_quantity
 
 EMERGENCY_REQUIREMENTS = {
-  "fire": EmergencyRequirement({
+  "fire_unit" or "fire": EmergencyRequirement({
     "fire_unit": 2,
     "police_unit": 1
   }),
-  "police": EmergencyRequirement({
+  "police_unit" or "police": EmergencyRequirement({
     "police_unit": 2
   }),
-  "ems": EmergencyRequirement({
+  "ems_unit" or "ems": EmergencyRequirement({
     "fire_unit": 1,
     "ems_unit": 1
   })
