154b6a5e93f3442d9faf698822f3e98e56d77a46
bobby <contact@bobbyobrien.com>
2025-02-16 01:34:53 -0600

    Listen on wide LAN

--- app/config.py
+++ app/config.py
@@ -4,4 +4,5 @@ Central configuration file including server settings, debug flags, etc.
 import os
 
 DEBUG = os.getenv("DEBUG", "true").lower() == "true"
+HOST = os.getenv("HOST", "0.0.0.0")
 PORT = int(os.getenv("PORT", 8727))
